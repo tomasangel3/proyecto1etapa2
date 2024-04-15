ce64a58f5a873c71d7e02e2b4f3eeebb600febee
Tomas Angel <t.angel@uniandes.edu.co>
2024-04-15 09:36:55 -0500

    Creacion del Pipeline y exportacion del modelo. Se agregan los documentos necesarios para realizar el API

--- DataModel.py
+++ DataModel.py
@@ -3,10 +3,9 @@ from pydantic import BaseModel
 class DataModel(BaseModel):
 
 # Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado.
-    review: str
-    class: int
+    Review: str
 
 #Esta función retorna los nombres de las columnas correspondientes con el modelo esxportado en joblib.
     def columns(self):
-        return ["Review","Class"]
+        return ["Review"]
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import Optional
-
+import DataModel
+import pandas as pd
 from fastapi import FastAPI
+from joblib import load
 
 app = FastAPI()
 
@@ -13,3 +15,11 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
    return {"item_id": item_id, "q": q}
+
+@app.post("/predict")
+def make_predictions(dataModel: DataModel):
+    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
+    df.columns = dataModel.columns()
+    model = load("'/Users/tomasangel/Documents/GitHub/proyecto1etapa2/data/modelo.joblib'")
+    result = model.predict(df)
+    return result
