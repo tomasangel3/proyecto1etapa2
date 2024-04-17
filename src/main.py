0d449cdeb2cdbe60c32c911e6e7a6c018fa6ac14
RAUL RINCON <r.rincon@uniandes.edu.co>
2024-04-17 14:01:25 -0500

    Refactor API endpoints and update model loading in main.py

--- src/main.py
+++ src/main.py
@@ -5,13 +5,20 @@ import pandas as pd
 import uvicorn
 import DataModel as dm
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, Request 
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from joblib import load
 import pandas as pd
 import uvicorn
 import DataModel as dm
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="../static"), name="static")
+templates = Jinja2Templates(directory="../templates")
+
+
 
 data=[]
 
@@ -22,17 +29,21 @@ async def create_review(reviews: List[dm.DataModel]):
       data.append(review.dict())
    return {"file": "successfully uploaded"}
 
-@app.get("/show/reviews")
-async def get_reviews():
-   return data
+@app.get("/show/reviews", response_class=HTMLResponse)
+async def get_reviews(request: Request):
+   return templates.TemplateResponse(
+      request=request, name="reviews.html", context={"data": data}
+   )
 
 @app.get("/predict")
 def make_predictions():
-    df = pd.DataFrame(data, columns=('Review'), index=[0])
-    df.columns = ('Review')
-    model = load("modelo.joblib")
-    result = model.predict(df)
-    return result
+    df = pd.DataFrame(data, columns=['Review'])
+    #for review in data :
+    #    print(review)
+    #    df = df.append({'Review':review}, ignore_index=True)
+    #model = load("../data/modelo.joblib")
+    #result = model.predict(df)
+    return df
 
 if __name__ == "__main__":
    uvicorn.run(app, host="127.0.0.1", port=8000)
