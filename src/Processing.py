6271582c7f7dc5a1565297f5f6d167884370f387
Raul Rincon <r.rincon@uniandes.edu.co>
2024-04-19 23:25:09 -0500

    palabras mas repetidas

---
+++ src/Processing.py
@@ -0,0 +1,86 @@
+import unicodedata
+import re
+import contractions
+import spacy
+import inflect
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords, wordnet
+from nltk.stem import SnowballStemmer, WordNetLemmatizer
+
+def tokenizer(text):
+    return word_tokenize(text, language="spanish")
+
+def preprocessor(text):
+     text = re.sub('<[^>]*>', '', text)
+     emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',
+                            text)
+     text = (re.sub('[\W]+', ' ', text.lower()) +
+             ' '.join(emoticons).replace('-', ''))
+     return text
+
+def remove_non_ascii(words):
+    """Remueve los caracteres de la lista de palabras tokenizadas que no esten en ASCII"""
+    new_words = []
+    for word in words:
+        if word is not None:
+            new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+            new_words.append(new_word)
+    return new_words
+
+def to_lowercase(words):
+    """Convierte todos los caracteres de la lista de palabras tokenizadas a minusculas"""
+    return [word.lower() for word in words]
+
+def remove_punctuation(words):
+    """Remueve la puntuacion de la lista de palabras tokenizadas"""
+    new_words = []
+    for word in words:
+        if word is not None:
+            new_word = re.sub(r'[^\w\s]', '', word)
+            if new_word != '':
+                new_words.append(new_word)
+    return new_words
+
+def replace_numbers(words):
+    """
+    Esta función toma una lista de palabras y reemplaza cada número en la lista con su representación textual.
+    """
+    p = inflect.engine()
+    processed_words = []
+    for word in words:
+        if word.isdigit():
+            word_as_text = p.number_to_words(word)
+            processed_words.append(word_as_text)
+        else:
+            processed_words.append(word)
+    return processed_words
+
+def remove_stopwords(words):
+    """Remueve las 'stop words' de la lista de palabras tokenizadas"""
+    stop_words = set(stopwords.words('spanish'))
+    return [word for word in words if word not in stop_words]
+
+def preprocessing(words):
+    words = to_lowercase(words)
+    words = replace_numbers(words)
+    words = remove_punctuation(words)
+    words = remove_non_ascii(words)
+    words = remove_stopwords(words)
+    return words
+
+
+
+def lemmatize_verbs(Palabras):
+    #Lemmatize verbs in list of tokenized words
+    lemmatizer = spacy.load("es_core_news_sm")
+    doc = lemmatizer(" ".join(Palabras))
+    return [token.lemma_ for token in doc]
+
+def limpiar(df_resultados):
+    df_resultados['text'] = df_resultados['text'].apply(contractions.fix)
+    df_resultados['Palabras'] = df_resultados['text'].apply(word_tokenize)
+    df_resultados['Palabras'].dropna()
+    df_resultados['Palabras'] = df_resultados['Palabras'].apply(preprocessing)
+    return df_resultados
--- src/main.py
+++ src/main.py
@@ -4,7 +4,11 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from matplotlib import pyplot as plt
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, ConfusionMatrixDisplay, f1_score
+from collections import Counter
+import nltk
+from nltk.corpus import stopwords
+import Processing
+import re
 import io
 import joblib
 import seaborn as sns
@@ -18,6 +22,7 @@ templates = Jinja2Templates(directory="../templates")
 
 data=[]
 list_predictions = []
+comun = None
 
 @app.post("/uploadfile/", response_class=HTMLResponse)
 async def create_upload_file(request: Request, file: UploadFile = File(...)):
@@ -37,7 +42,13 @@ async def get_reviews(request: Request):
       request=request, name="reviews.html", context={"data": data})
 
 @app.get("/predict", response_class=HTMLResponse)
-def make_predictions(request: Request):
+async def make_predictions(request: Request):
+
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('spanish'))
+    nltk.download('punkt')
+    nltk.download('vader_lexicon')
+
     df = pd.DataFrame(data, columns=['Review'])
 
     # Cargamos nuestro Pipeline de jupyter 
@@ -47,15 +58,29 @@ def make_predictions(request: Request):
     # Guardamos la prediccion realizada en una lista de diccionarios para imprimirla 
     for text, prediction in zip(df['Review'], predict):
         list_predictions.append({"text": text, "prediction": prediction})
+
     # Plopteamos 
     df_resultados = pd.DataFrame(list_predictions)
     generar_plots(df_resultados)
+
+    # Palabras comunes 
+    palabras_comunes(df_resultados)
+    df_resultados['Conteo'] = df_resultados['Palabras'].apply(lambda x: Counter(x))
+    # Para cada lista, encontramos la palabra más común y su conteo
+    df_resultados['Palabra_mas_comun'] = df_resultados['Conteo'].apply(lambda x: x.most_common(1)[0] if x else None)
+    grupos = df_resultados.groupby('prediction')['Palabras'].sum()
+    global comun
+    comun = {prediction: Counter(grupo).most_common(20) for prediction, grupo in grupos.items()}
 
     return templates.TemplateResponse(
       request=request, name="prediccion.html", context={"predict": list_predictions})
 
 @app.get("/tablero", response_class=HTMLResponse)
 def obtener_tablero(request: Request):
+    # sacamos las palabras mas comunes para cada clasificacion
+    print(comun)
+    # haz una tabla  por cada llave
+    
     return templates.TemplateResponse('tablero.html', {'request': request})
 
 def generar_plots(df_resultados):
@@ -82,6 +107,9 @@ def generar_plots(df_resultados):
     fig = ex.get_figure()
     fig.savefig("../static/scatterplot.png")
     plt.clf()  
+
+def palabras_comunes(df_resultados):
+    Processing.limpiar(df_resultados)
 
 if __name__ == "__main__":
    uvicorn.run(app, host="127.0.0.1", port=8000)
